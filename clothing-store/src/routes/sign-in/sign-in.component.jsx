6cc956587e9c195be7de98196b9597d082f9106b
Gegzz <93631562+Gegzz@users.noreply.github.com>
2022-07-21 22:51:03 +0400

    created sign-up component, form input component and Button component

---
+++ clothing-store/src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,23 @@
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
+import "../navigation/navigation.styles.scss";
+
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+export const SignIn = () => {
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+  };
+
+  return (
+    <div>
+      <h1>Sign In Page</h1>
+      <button onClick={logGoogleUser}>Sign in With Google Pop Up !</button>
+      <SignUpForm/>
+    </div>
+  );
+};
+
+export default SignIn;
---
+++ clothing-store/src/utils/firebase/firebase.utils.jsx
@@ -0,0 +1,73 @@
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+  createUserWithEmailAndPassword,
+} from "firebase/auth";
+
+import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBx0iFdhdLDCScccbrell9MzGYkoq6_bKo",
+  authDomain: "crown-clothing-db-3e4c7.firebaseapp.com",
+  projectId: "crown-clothing-db-3e4c7",
+  storageBucket: "crown-clothing-db-3e4c7.appspot.com",
+  messagingSenderId: "440142185542",
+  appId: "1:440142185542:web:a42acf3c74bfdbe355b647",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+const googleProvider = new GoogleAuthProvider();
+googleProvider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () =>
+  signInWithPopup(auth, googleProvider);
+export const signInWithGoogleRedirect = () =>
+  signInWithRedirect(auth, googleProvider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth,additionalInformation = { }) => {
+    if(!userAuth) return;
+  const userDocRef = doc(db, "users", userAuth.uid);
+
+  console.log(userDocRef);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  console.log(userSnapshot);
+  console.log(userSnapshot.exists());
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+        ...additionalInformation,
+      });
+    } catch (error) {
+      console.log("error creating the user", error.message);
+    }
+  }
+
+  return userDocRef;
+};
+
+export const createAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+ return await createUserWithEmailAndPassword(auth, email, password);
+};
+
+// if user data does not exist
+// create / set the document with the data from userauth in my collection
